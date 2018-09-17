e28a5849c030ebddb007ef0a5f4ac3eee956bfd4
maxdk9 <maxdk9@gmail.com>
2018-09-17 16:39:28 +0300

    flickrrecycleviewadapter

--- flickrbrowser/app/src/main/java/mazzy/and/udnugat1/flickbrowser/FlickrRecyclerViewAdapter.java
+++ flickrbrowser/app/src/main/java/mazzy/and/udnugat1/flickbrowser/FlickrRecyclerViewAdapter.java
@@ -1,8 +1,33 @@
 package mazzy.and.udnugat1.flickbrowser;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
-public class FlickrRecyclerViewAdapter  {
+import java.util.List;
 
+public class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewHolder> {
+    private static final String TAG = "FlickrRecyclerViewAdapt";
+
+    private List<Photo> mPhotoList;
+    private Context mContext;
+    private int YouCanDeleteThisVariable;
+
+    
+
+    @Override
+    public FlickrRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrRecyclerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
