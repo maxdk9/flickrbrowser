db9176fa403b7d3f0cccd479b4ef9d5af6be1ced
maxdk9 <maxdk9@gmail.com>
2018-10-05 17:45:09 +0300

    164

--- flickrbrowser/app/src/main/java/mazzy/and/udnugat1/flickbrowser/SearchActivity.java
+++ flickrbrowser/app/src/main/java/mazzy/and/udnugat1/flickbrowser/SearchActivity.java
@@ -1,15 +1,55 @@
 package mazzy.and.udnugat1.flickbrowser;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 
 
 public class SearchActivity extends BaseActivity {
+    private static final String TAG = "SearchActivity";
+
+    private SearchView mSearchView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG,"onCreateOptionsMenu starts");
+        getMenuInflater().inflate(R.menu.menu_search,menu);
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        mSearchView= (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        mSearchView.setSearchableInfo(searchableInfo);
+
+//        Log.d(TAG, "onCreateOptionsMenu " + getComponentName());
+//        Log.d(TAG, "onCreateOptionsMenu " + mSearchView.getQueryHint());
+//        Log.d(TAG, "onCreateOptionsMenu " + searchableInfo.toString());
+        Log.d(TAG, "onCreateOptionsMenu " + "returns true");
+        mSearchView.setIconified(false);
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.d(TAG, "onQueryTextSubmit called");
+                mSearchView.clearFocus();
+                finish();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
